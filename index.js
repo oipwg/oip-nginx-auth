62ef984aa92d277dfa6b58030557f39ae8bace6d
Sky Young <sky@alexandria.io>
2018-08-16 12:01:53 -0700

    Add second

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const app = express()
 const config = require("./config.json")
 
 // Set the API to not care about body size
-app.use(bodyParser.json({limit: '10240mb'}));
+app.use(bodyParser.json({limit: '2048mb'}));
+app.use(bodyParser.urlencoded({limit: '2048mb', extended: true}));
 
 app.get('/auth_request', (req, res) => {
 	var original_request_type = req.get("X-Original-Method")
